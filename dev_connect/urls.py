d5f5e7adf54a62038f963c2374f6c07625f8450a
Tay J Solis <tayjsolis@gmail.com>
2018-10-26 10:39:16 -0700

    Added projects to profile and created project details pathway

--- dev_connect/urls.py
+++ dev_connect/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<username>/', views.student_profile, name='student_profile'),
+    path('students/<username>/projects/<title>', views.project_details, name='project_details'),
     path('', views.all_profiles, name='all_profiles')
 ]
--- dev_connect/views.py
+++ dev_connect/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import StudentProfile, Project
 from django.contrib import auth
 from django.contrib.auth.models import User
 
 # Create your views here.
 def student_profile(request, username):
-    student = User.objects.get(username=username)
-    profile = StudentProfile.objects.get(user_id=student.id)
-    return render(request, 'dev_connect/profile.html', {'student': student, 'profile': profile})
+    student = get_object_or_404(User, username=username)
+    profile = get_object_or_404(StudentProfile, user_id=student.id)
+    projects = Project.objects.filter(user_id=student.id)
+    return render(request, 'dev_connect/profile.html', {'student': student, 'profile': profile, 'projects': projects})
+
+def project_details(request, username, title):
+    student = get_object_or_404(User, username=username)
+    project = Project.objects.filter(title=title)
+    return render(request, 'dev_connect/project_details.html', {'student': student, 'project': project})
 
 def all_profiles(request):
     profiles = StudentProfile.objects.all()
