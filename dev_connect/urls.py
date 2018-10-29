2e19b62de69ebd50db87177123639e329c7465fc
Tay J Solis <tayjsolis@gmail.com>
2018-10-29 14:12:15 -0700

    Add and edit projects

--- dev_connect/urls.py
+++ dev_connect/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('<username>/', views.student_profile, name='student_profile'),
     path('<username>/projects/<title>', views.project_details, name='project_details'),
     path('', views.all_profiles, name='all_profiles'),
-    # path('profile/add', views.add_profile, name='add_profile'),
-    path('profile/edit', views.edit_profile, name='edit_profile')
+    path('projects/add', views.add_project, name='add_project'),
+    path('projects/<title>/edit', views.edit_project, name='edit_project'),
+    path('profile/edit', views.edit_profile, name='edit_profile'),
 ]
